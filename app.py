0db5c0896a95a671ecb73932395fcb985cc41474
wilberandresm <wilberandresm@hotmail.com>
2019-09-03 22:30:27 -0500

    singleton
    acceso a la misma dirección de memoria

--- app.py
+++ app.py
@@ -6,23 +6,41 @@ app = Flask(__name__, static_url_path='/static')
 
 app.config['SQLALCHEMY_DATABASE_URI']= "sqlite:///database/bd.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
 app.secret_key = 'mysecretkey'
+db=SQLAlchemy(app)
 
- 
+class Singleton(object):
+	_instance = None
+	
+	def __new__(cls, *args, **kwargs):
+		if not cls._instance:
+			cls._instance = object.__new__(cls, *args, **kwargs)
+		
+		return cls._instance
+class listaC(Singleton):
+     t=[]
+     datos= db.engine.execute('select * from users;')
+     for row in datos:
+          t.append(row)      
+lista=listaC()
+l=listaC()
+
+lis=l.t
+lista1=lista.t
+
 @app.route("/", methods=['GET'])
 def inicio():
     return render_template('inicio.html')
 
 @app.route("/users/list",  methods=['GET'])
-def lista():
-    estudiantes = db.engine.execute("SELECT * FROM users")
-    return render_template('lista.html', data=estudiantes)
+def lista_vista():
+    print (hex(id(lista1)))
+    return render_template('lista.html', data=lista1)
 
 @app.route('/api/v1/users/')
 def tres():
-     data= db.engine.execute('select * from users')
+     print (hex(id(lis)))
+     data=lis
      otrcosa=json.dumps({'Json': [dict(row) for row in data]})
      f=open('archivo.json','w')
      f.write(otrcosa)
